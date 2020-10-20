35360e137f9f67d2334cb3e9b01c396f8bdc07f1
Tushargh29 <tushargholap29@gmail.com>
2020-10-20 22:35:15 +0200

    Extracting the reference data for Drag and Lift coefficients issue #4

--- extracting_the_reference_data/Compressibility_Cl.py
+++ extracting_the_reference_data/Compressibility_Cl.py
@@ -1,45 +1,48 @@
 # Importing the different libraries
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
+import numpy as np
 from matplotlib import pyplot as plt
+from scipy.interpolate import interp1d
+
+# increase resolution and use Latex rendering
+mpl.rcParams['figure.dpi'] = 160
+mpl.rc('text', usetex=True)
 
 
 # Reading the data from .csv files
-data = pd.read_csv('Cl(0)-cl(3).csv',delimiter=',')                     # Reading the Cl data for alpha = 0 to alpha = 3
-data1 = pd.read_csv('Cl(4)-Cl(6).csv',delimiter=',')                    # Reading the Cl data for alpha = 4 to alpha = 6
+data = pd.read_csv('Cl(0)-Cl(3).csv', delimiter=',')                     # Reading the Cl data for alpha = 0 to alpha = 3
+data1 = pd.read_csv('Cl(4)-Cl(6).csv', delimiter=',')                    # Reading the Cl data for alpha = 4 to alpha = 6
 
 
-# Assigning different columns of the data into different series
-alpha_zero = data['Cl_0']
-alpha_one = data['Cl_1']
-alpha_two = data['Cl_2']
-alpha_three = data['Cl_3']
+# Creating plot
+fig, ax = plt.subplots(1, 1, figsize=(8, 5))
 
-alpha_four = data1['Cl_4']
-alpha_five = data1['Cl_5']
-alpha_six = data1['Cl_6']
+markers = ["x", "<", ">", "+", "o", "*", "^"]
+marker_size = 60
+color = "k"
 
-Mach_number = data['x']
-Mach_number1 = data1['x1']
+cl = [data['Cl_0'], data['Cl_1'], data['Cl_2'], data['Cl_3'], data1['Cl_4'], data1['Cl_5'], data1['Cl_6']]
+ma = [data['x'], data['x'], data['x'], data['x'], data1['x1'], data1['x1'], data1['x1']]
 
+for i in range(len(cl)):
+    ax.scatter(ma[i], cl[i], marker=markers[i], color=color, s=marker_size, label=r"$\alpha={:1d}^\circ$".format(i))
+    cubic_spline = interp1d(ma[i].values, cl[i].values, kind='cubic')
+    ma_spline = np.linspace(np.min(ma[i].values), np.max(ma[i].values), 100)
+    if i==0:
+        ax.plot(ma_spline, cubic_spline(ma_spline), ls=":", color=color, label="cubic spline")
+    else:
+        ax.plot(ma_spline, cubic_spline(ma_spline), ls=":", color=color)
 
+ax.set_xlabel(r"$Ma$")
+ax.set_ylabel(r"$c_l$")
+ax.set_title(r'Effect of compressibility on the lift of NACA0012-34 airfoil')
+ax.legend()
 
-# Plotting
-plt.plot(Mach_number,alpha_zero, label='alpha = 0')
-plt.plot(Mach_number,alpha_one,label='alpha = 1')
-plt.plot(Mach_number,alpha_two,label='alpha = 2')
-plt.plot(Mach_number,alpha_three,label='alpha = 3')
-plt.plot(Mach_number1,alpha_four,label='alpha = 4')
-plt.plot(Mach_number1,alpha_five,label='alpha = 5')
-plt.plot(Mach_number1,alpha_six,label='alpha = 6')
-
-
-
-plt.legend()
-plt.xlabel('Mach Number (M)')
-plt.ylabel('Lift Coefficient (CL)')
-plt.title('Effect of compressibility on the Lift of the NACA 0012-34 airfoil')
-
-
+# save as PDF for general usage, e.g., inclusion in Latex documents
+plt.savefig("cl_over_ma_reference.pdf", bbox_inches="tight")
+# save as svg for web, e.g., Github, websites, ...
+plt.savefig("cl_over_ma_reference.svg", bbox_inches="tight")
+# show for convenience
 plt.show()
-
